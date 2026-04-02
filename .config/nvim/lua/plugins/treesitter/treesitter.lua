b499621b70ac086a2a1fc8aed6fe895951094c19
blru <44450511+blru@users.noreply.github.com>
2026-04-02 19:41:24 -0400

    impr(nvim): made treesitter automatically add folds

--- .config/nvim/lua/plugins/treesitter/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter/treesitter.lua
@@ -42,10 +42,8 @@ return {
             pattern = languages,
             callback = function()
                 vim.treesitter.start()
-
-                vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-                vim.bo.indentexpr =
-                    "v:lua.require'nvim-treesitter'.indentexpr()"
+                vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                vim.wo[0][0].foldmethod = 'expr'
             end,
         })
     end,
